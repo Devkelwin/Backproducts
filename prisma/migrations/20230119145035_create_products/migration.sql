f5aa480f6ffa6a6df03fdf4e8745f88947d75043
KingKelwin <kelwincarlos123@gmail.com>
2023-08-05 16:51:07 -0300

    first commit

--- prisma/migrations/20230119145035_create_products/migration.sql
+++ prisma/migrations/20230119145035_create_products/migration.sql
@@ -1,11 +1,11 @@
 -- CreateTable
 CREATE TABLE "products" (
     
-    "product_id" TEXT NOT NULL
-    "product_name" TEXT NOT NULL
-    "actual_price" TEXT NOT NULL
-    "img_link" TEXT NOT NULL
-    "about_product" TEXT NOT NULL
+    "product_id" TEXT NOT NULL,
+    "product_name" TEXT NOT NULL,
+    "actual_price" TEXT NOT NULL,
+    "img_link" TEXT NOT NULL,
+    "about_product" TEXT NOT NULL,
 
     CONSTRAINT "products_pkey" PRIMARY KEY ("product_id")
 );
